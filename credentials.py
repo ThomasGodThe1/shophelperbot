fa245c9937d8df8eece5ef69eb6b5ec700d939bd
Thomas tesfu <thomastkf01@gmail.com>
2022-02-27 21:04:03 +0300

    Made the credentials

--- credentials.py
+++ credentials.py
@@ -1,7 +1,7 @@
 import os
 
 channel_id=os.environ.get("channel_id")
-bot_token=os.environ.get("bot_token")
-key=os.environ.get("key")
+bot_token='5003859809:AAEP6KJhsBTw3fe6HRq9R56P0SzbZB7yDUw'
+key='5003859809:AAEP6KJhsBTw3fe6HRq9R56P0SzbZB7yDUws'
 verifierchat_id=int(os.environ.get("verifierchat_id"))
 # verifierchat_id=693856597
